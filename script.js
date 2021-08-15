12440bdc07df20b97e14a9ed10467831ba524b5c
Kaushalendra Pandey <pandeysid713@gmail.com>
2021-08-16 00:50:41 +0530

    feat: added api fetching and select box

---
+++ script.js
@@ -0,0 +1,33 @@
+inputText = document.getElementById("input-text")
+button = document.getElementById("button")
+let translatedBox = document.getElementById("translated-text")
+let language = document.getElementById("language")
+// default language.
+let curLanguage = "pirate"
+
+
+const getEnteredText = async () => {
+    const entered_text = inputText.value
+    let translatedText = await getTranslatedText(entered_text)
+    addToTranslatedBox(translatedText === undefined ? "Try again after some time" : translatedText)
+}
+
+const getTranslatedText = async (entered_text) => {
+    let data = await fetch(`https://api.funtranslations.com/translate/${curLanguage}.json?text=${entered_text}`)
+    let res = await data.json()
+    let fetchedText = res?.contents?.translated
+    console.log(fetchedText)
+    return fetchedText
+}
+
+const addToTranslatedBox = (translatedText) => {
+    translatedBox.innerHTML = ""    
+    para = document.createElement("p")
+    para.innerHTML = translatedText
+    translatedBox.appendChild(para)
+}
+
+const onLanguageChange = () => {
+    console.log(language.value)
+    curLanguage = language.value
+}
